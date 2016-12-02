d54edffe54aaced8d901284501181c2bc11e5cc4
Olivia <reach.olivialegge@gmail.com>
2016-12-02 13:13:58 -0800

    wave 3

--- src/app/views/quote_view.js
+++ src/app/views/quote_view.js
@@ -9,14 +9,29 @@ var QuoteView = Backbone.View.extend({
       this.template = options.template;
    },
 
- render: function() {
-   var html = this.template(this.quote);
 
-   this.$el.html(html);
+  render: function() {
+     var html = this.template(this.quote);
 
-   // Enable chained calls
-   return this;
- }
+     this.$el.html(html);
+
+     // Enable chained calls
+     return this;
+  },
+
+  events:{
+    'click .success': 'increasePrice',
+    'click .alert': 'decreasePrice',
+
+  },
+  increasePrice: function(){
+    this.quote.price += 1
+    this.render();
+  },
+  decreasePrice: function(){
+    this.quote.price -= 1
+    this.render();
+  }
 });
 
 export default QuoteView;
